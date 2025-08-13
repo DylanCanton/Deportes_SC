3feb8e2c42784376cf410b33761980f23dbf6d71
yeudy <yeudyperalta15@gmail.com>
2025-08-12 18:33:20 -0600

    cambios interfaz (falta)

--- Deportes_SC/Presentacion/FrmPrincipal.Designer.cs
+++ Deportes_SC/Presentacion/FrmPrincipal.Designer.cs
@@ -39,16 +39,15 @@
             this.btn_Estadisticas = new System.Windows.Forms.Button();
             this.pnl_superior = new System.Windows.Forms.Panel();
             this.Minimized = new System.Windows.Forms.PictureBox();
-            this.pnl_derecha = new System.Windows.Forms.Panel();
             this.Exit = new System.Windows.Forms.PictureBox();
             this.pnl_izquierda = new System.Windows.Forms.Panel();
-            this.pnl_inferior = new System.Windows.Forms.Panel();
             this.btn_CargarDatos = new System.Windows.Forms.Button();
+            this.pnl_contenedor = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.picFondo)).BeginInit();
             this.pnl_superior.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Minimized)).BeginInit();
-            this.pnl_derecha.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Exit)).BeginInit();
+            this.pnl_izquierda.SuspendLayout();
             this.SuspendLayout();
             // 
             // picFondo
@@ -68,7 +67,7 @@
             this.btn_CerrarSesion.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_CerrarSesion.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_CerrarSesion.Image = ((System.Drawing.Image)(resources.GetObject("btn_CerrarSesion.Image")));
-            this.btn_CerrarSesion.Location = new System.Drawing.Point(136, 657);
+            this.btn_CerrarSesion.Location = new System.Drawing.Point(26, 661);
             this.btn_CerrarSesion.Name = "btn_CerrarSesion";
             this.btn_CerrarSesion.Size = new System.Drawing.Size(259, 68);
             this.btn_CerrarSesion.TabIndex = 9;
@@ -83,7 +82,7 @@
             this.btn_Jugadores.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_Jugadores.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_Jugadores.Image = ((System.Drawing.Image)(resources.GetObject("btn_Jugadores.Image")));
-            this.btn_Jugadores.Location = new System.Drawing.Point(136, 241);
+            this.btn_Jugadores.Location = new System.Drawing.Point(26, 312);
             this.btn_Jugadores.Name = "btn_Jugadores";
             this.btn_Jugadores.Size = new System.Drawing.Size(259, 68);
             this.btn_Jugadores.TabIndex = 8;
@@ -98,7 +97,7 @@
             this.btn_Torneos.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_Torneos.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_Torneos.Image = ((System.Drawing.Image)(resources.GetObject("btn_Torneos.Image")));
-            this.btn_Torneos.Location = new System.Drawing.Point(136, 48);
+            this.btn_Torneos.Location = new System.Drawing.Point(26, 142);
             this.btn_Torneos.Name = "btn_Torneos";
             this.btn_Torneos.Size = new System.Drawing.Size(259, 68);
             this.btn_Torneos.TabIndex = 10;
@@ -113,7 +112,7 @@
             this.btn_Equipos.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_Equipos.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_Equipos.Image = ((System.Drawing.Image)(resources.GetObject("btn_Equipos.Image")));
-            this.btn_Equipos.Location = new System.Drawing.Point(64, 147);
+            this.btn_Equipos.Location = new System.Drawing.Point(26, 228);
             this.btn_Equipos.Name = "btn_Equipos";
             this.btn_Equipos.Size = new System.Drawing.Size(259, 68);
             this.btn_Equipos.TabIndex = 11;
@@ -128,7 +127,7 @@
             this.btn_Usuarios.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_Usuarios.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_Usuarios.Image = ((System.Drawing.Image)(resources.GetObject("btn_Usuarios.Image")));
-            this.btn_Usuarios.Location = new System.Drawing.Point(64, 540);
+            this.btn_Usuarios.Location = new System.Drawing.Point(26, 573);
             this.btn_Usuarios.Name = "btn_Usuarios";
             this.btn_Usuarios.Size = new System.Drawing.Size(259, 68);
             this.btn_Usuarios.TabIndex = 13;
@@ -143,7 +142,7 @@
             this.btn_Partidos.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_Partidos.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_Partidos.Image = ((System.Drawing.Image)(resources.GetObject("btn_Partidos.Image")));
-            this.btn_Partidos.Location = new System.Drawing.Point(64, 342);
+            this.btn_Partidos.Location = new System.Drawing.Point(26, 400);
             this.btn_Partidos.Name = "btn_Partidos";
             this.btn_Partidos.Size = new System.Drawing.Size(259, 68);
             this.btn_Partidos.TabIndex = 14;
@@ -158,7 +157,7 @@
             this.btn_Estadisticas.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btn_Estadisticas.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btn_Estadisticas.Image = ((System.Drawing.Image)(resources.GetObject("btn_Estadisticas.Image")));
-            this.btn_Estadisticas.Location = new System.Drawing.Point(136, 433);
+            this.btn_Estadisticas.Location = new System.Drawing.Point(26, 487);
             this.btn_Estadisticas.Name = "btn_Estadisticas";
             this.btn_Estadisticas.Size = new System.Drawing.Size(259, 68);
             this.btn_Estadisticas.TabIndex = 15;
@@ -170,10 +169,12 @@
             // pnl_superior
             // 
             this.pnl_superior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pnl_superior.Controls.Add(this.Exit);
             this.pnl_superior.Controls.Add(this.Minimized);
-            this.pnl_superior.Location = new System.Drawing.Point(26, 0);
+            this.pnl_superior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_superior.Location = new System.Drawing.Point(0, 0);
             this.pnl_superior.Name = "pnl_superior";
-            this.pnl_superior.Size = new System.Drawing.Size(929, 28);
+            this.pnl_superior.Size = new System.Drawing.Size(981, 28);
             this.pnl_superior.TabIndex = 16;
             // 
             // Minimized
@@ -181,7 +182,7 @@
             this.Minimized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Minimized.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Minimized.Image = ((System.Drawing.Image)(resources.GetObject("Minimized.Image")));
-            this.Minimized.Location = new System.Drawing.Point(887, 3);
+            this.Minimized.Location = new System.Drawing.Point(920, 0);
             this.Minimized.Name = "Minimized";
             this.Minimized.Size = new System.Drawing.Size(25, 25);
             this.Minimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -189,21 +190,12 @@
             this.Minimized.TabStop = false;
             this.Minimized.Click += new System.EventHandler(this.Minimized_Click);
             // 
-            // pnl_derecha
-            // 
-            this.pnl_derecha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.pnl_derecha.Controls.Add(this.Exit);
-            this.pnl_derecha.Location = new System.Drawing.Point(953, 0);
-            this.pnl_derecha.Name = "pnl_derecha";
-            this.pnl_derecha.Size = new System.Drawing.Size(28, 750);
-            this.pnl_derecha.TabIndex = 17;
-            // 
             // Exit
             // 
             this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Exit.Image = ((System.Drawing.Image)(resources.GetObject("Exit.Image")));
-            this.Exit.Location = new System.Drawing.Point(-2, 3);
+            this.Exit.Location = new System.Drawing.Point(951, 0);
             this.Exit.Name = "Exit";
             this.Exit.Size = new System.Drawing.Size(27, 25);
             this.Exit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -214,18 +206,18 @@
             // pnl_izquierda
             // 
             this.pnl_izquierda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.pnl_izquierda.Location = new System.Drawing.Point(0, 0);
+            this.pnl_izquierda.Controls.Add(this.btn_Torneos);
+            this.pnl_izquierda.Controls.Add(this.btn_Jugadores);
+            this.pnl_izquierda.Controls.Add(this.btn_CerrarSesion);
+            this.pnl_izquierda.Controls.Add(this.btn_Equipos);
+            this.pnl_izquierda.Controls.Add(this.btn_Usuarios);
+            this.pnl_izquierda.Controls.Add(this.btn_Estadisticas);
+            this.pnl_izquierda.Controls.Add(this.btn_Partidos);
+            this.pnl_izquierda.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_izquierda.Location = new System.Drawing.Point(0, 28);
             this.pnl_izquierda.Name = "pnl_izquierda";
-            this.pnl_izquierda.Size = new System.Drawing.Size(28, 750);
+            this.pnl_izquierda.Size = new System.Drawing.Size(306, 734);
             this.pnl_izquierda.TabIndex = 18;
-            // 
-            // pnl_inferior
-            // 
-            this.pnl_inferior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.pnl_inferior.Location = new System.Drawing.Point(0, 740);
-            this.pnl_inferior.Name = "pnl_inferior";
-            this.pnl_inferior.Size = new System.Drawing.Size(981, 22);
-            this.pnl_inferior.TabIndex = 19;
             // 
             // btn_CargarDatos
             // 
@@ -240,23 +232,23 @@
             this.btn_CargarDatos.UseVisualStyleBackColor = false;
             this.btn_CargarDatos.Click += new System.EventHandler(this.btn_CargarDatos_Click);
             // 
+            // pnl_contenedor
+            // 
+            this.pnl_contenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_contenedor.Location = new System.Drawing.Point(306, 28);
+            this.pnl_contenedor.Name = "pnl_contenedor";
+            this.pnl_contenedor.Size = new System.Drawing.Size(675, 734);
+            this.pnl_contenedor.TabIndex = 21;
+            // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(981, 762);
+            this.Controls.Add(this.pnl_contenedor);
             this.Controls.Add(this.btn_CargarDatos);
-            this.Controls.Add(this.pnl_inferior);
             this.Controls.Add(this.pnl_izquierda);
-            this.Controls.Add(this.pnl_derecha);
             this.Controls.Add(this.pnl_superior);
-            this.Controls.Add(this.btn_Estadisticas);
-            this.Controls.Add(this.btn_Partidos);
-            this.Controls.Add(this.btn_Usuarios);
-            this.Controls.Add(this.btn_Equipos);
-            this.Controls.Add(this.btn_Torneos);
-            this.Controls.Add(this.btn_CerrarSesion);
-            this.Controls.Add(this.btn_Jugadores);
             this.Controls.Add(this.picFondo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmPrincipal";
@@ -265,8 +257,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.picFondo)).EndInit();
             this.pnl_superior.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Minimized)).EndInit();
-            this.pnl_derecha.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Exit)).EndInit();
+            this.pnl_izquierda.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -282,11 +274,10 @@
         private System.Windows.Forms.Button btn_Partidos;
         private System.Windows.Forms.Button btn_Estadisticas;
         private System.Windows.Forms.Panel pnl_superior;
-        private System.Windows.Forms.Panel pnl_derecha;
         private System.Windows.Forms.Panel pnl_izquierda;
-        private System.Windows.Forms.Panel pnl_inferior;
         private System.Windows.Forms.PictureBox Minimized;
         private System.Windows.Forms.PictureBox Exit;
         private System.Windows.Forms.Button btn_CargarDatos;
+        private System.Windows.Forms.Panel pnl_contenedor;
     }
 }
